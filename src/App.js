ef3f41eb2a7805a2274def2b377937d323c01e0c
Nathan Jean <nathanjean@kyrosagency.com>
2021-02-10 15:36:44 -0500

    refactor components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container, Grid, Icon, Segment, Statistic } from 'semantic-ui-react'
 import './App.css';
 import DisplayBalance from './components/DisplayBalance.js';
 import DisplayBalances from './components/DisplayBalances.js';
+import EntryLine from './components/EntryLine';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 
@@ -18,44 +19,8 @@ function App() {
 
 			<MainHeader title="History" type="h3"/>
 
-			<Segment color="red">
-				<Grid columns={3} textAlign="right">
-					<Grid.Row>
-						<Grid.Column width={10} textAlign="left">Something</Grid.Column>
-						<Grid.Column width={3} textAlign="right"> $10.00</Grid.Column>
-						<Grid.Column width={3}>
-							<Icon name="edit" bordered/>
-							<Icon name="trash"bordered/>
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-			</Segment>
-
-			<Segment color="green">
-				<Grid columns={3} textAlign="right">
-					<Grid.Row>
-						<Grid.Column width={10} textAlign="left">Something Else</Grid.Column>
-						<Grid.Column width={3} textAlign="right"> $100.00</Grid.Column>
-						<Grid.Column width={3}>
-							<Icon name="edit" bordered/>
-							<Icon name="trash"bordered/>
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-			</Segment>
-
-			<Segment color="red">
-				<Grid columns={3} textAlign="right">
-					<Grid.Row>
-						<Grid.Column width={10} textAlign="left">Something</Grid.Column>
-						<Grid.Column width={3} textAlign="right"> $20.00</Grid.Column>
-						<Grid.Column width={3}>
-							<Icon name="edit" bordered/>
-							<Icon name="trash"bordered/>
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-			</Segment>
+			<EntryLine description="Income" value="$10"/>
+			<EntryLine description="Expense" value="$10" isExpense/>
 
 			<MainHeader title="Add New Transaction" type="h3"/>
 

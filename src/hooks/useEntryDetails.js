c1bbac44df84fbb2780c1ad463a4066e249b1122
kyros <nathanjean@kyrosagency.com>
2021-02-12 11:02:43 -0500

    call 2 dispatches

--- src/hooks/useEntryDetails.js
+++ src/hooks/useEntryDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import {useDispatch} from 'react-redux'
 import { addEntryRedux, updateEntryRedux } from '../actions/entries.actions';
 import {v4 as uuidv4} from 'uuid'
+import { closeEditModal } from '../actions/modals.actions';
 
 function useEntryDetails(desc="", val="", isExp=true) {
 	const [description, setDescription] = useState(desc);
@@ -20,6 +21,7 @@ function useEntryDetails(desc="", val="", isExp=true) {
 			id, 
 			{ id, description, value, isExpense }
 		))
+		dispatch(closeEditModal())
 	}
 
 	const addEntry = () => {

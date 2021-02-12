55f0a7cfc9658e858ae90d84bb6413c35b4372af
kyros <nathanjean@kyrosagency.com>
2021-02-12 12:07:27 -0500

    use call

--- src/sagas/testSaga.js
+++ src/sagas/testSaga.js
@@ -1,9 +1,17 @@
-import {delay, put, take} from 'redux-saga/effects'
+import {delay, put, take, call} from 'redux-saga/effects'
+
+function double(number) {
+	return number * 2
+}
 
 export function* testSaga() {
 	while(true) {
 		console.log('Starting Saga')
 		const state = yield take('TEST_MESSAGE')
+		const a = yield call(double, 2)
+		console.log(a)
+		const b = yield double(3)
+		console.log(b)
 		console.log('Finish saga function', state)
 	}
 }

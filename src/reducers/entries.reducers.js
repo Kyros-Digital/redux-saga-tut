f3190244e18928ed0c0aa526884ad92988c3636b
Nathan Jean <nathanjean@kyrosagency.com>
2021-02-12 23:17:42 -0500

    split the backend

--- src/reducers/entries.reducers.js
+++ src/reducers/entries.reducers.js
@@ -12,10 +12,11 @@ export const entriesReducer = (state = initialEntries, action) => {
 		case entryTypes.REMOVE_ENTRY: 
 			newEntries = state.filter(entry => entry.id !== action.payload.id)
 			return newEntries
+		case entryTypes.POPULATE_ENTRIES_DETAILS:
 		case entryTypes.UPDATE_ENTRY: 
 			newEntries = [...state]
 			const index = newEntries.findIndex(entry => entry.id === action.payload.id)
-			newEntries[index] = {...action.payload.entry}
+			newEntries[index] = {...newEntries[index], ...action.payload.entry}
 			return newEntries
 		default:
 			return state;
--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,4 +1,4 @@
-import { call, put, take } from "redux-saga/effects";
+import { call, fork, put, take } from "redux-saga/effects";
 import entryTypes from '../actions/entries.actions'
 import axios from 'axios'
 import types from "../actions/entries.actions";
@@ -9,4 +9,16 @@ export function* getAllEntries() {
 	const result = yield call(axios, 'http://localhost:3001/entries')
 	console.log(result)
 	yield put({type: types.POPULATE_ENTRIES, payload: result.data})
+}
+export function* getEntryDetails(id) {
+	const { data } = yield call(axios, `http://localhost:3001/values/${id}`)
+	console.log(data)
+	yield put({type: entryTypes.POPULATE_ENTRIES_DETAILS, payload: {id, entry:data}})
+}
+export function* getAllEntriesDetails() {
+	const { payload } = yield take(entryTypes.POPULATE_ENTRIES)
+	for (let index = 0; index < payload.length; index++) {
+		const entry = payload[index];
+		yield fork(getEntryDetails, entry.id)
+	}
 }

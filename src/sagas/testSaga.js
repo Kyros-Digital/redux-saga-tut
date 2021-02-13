2c2c415259d42d9ef0b0f1c02efae87ed2de7f83
Nathan Jean <nathanjean@kyrosagency.com>
2021-02-12 23:02:17 -0500

    run test saga with multiple forks

--- src/sagas/testSaga.js
+++ src/sagas/testSaga.js
@@ -1,4 +1,4 @@
-import {delay, put, take, call} from 'redux-saga/effects'
+import {delay, put, take, call, fork} from 'redux-saga/effects'
 
 function double(number) {
 	return number * 2
@@ -16,9 +16,26 @@ export function* testSaga() {
 	}
 }
 
+function* doNothing() {
+	console.log('I have been called')
+	yield delay(500)
+	console.log("I am doing nothing")
+}
+
+export function* testSagaFork() {
+	while(true) {
+		yield take('TEST_MESSAGE_2')
+		yield delay(1000)
+		yield fork(doNothing)
+		yield fork(doNothing)
+		yield fork(doNothing)
+		
+	}
+}
+
 export function* dispatchTest() {
 	while(true) {
-		yield delay(1000)
-		yield put({type: "TEST_MESSAGE", payload: 1000})
+		yield delay(5000)
+		yield put({type: "TEST_MESSAGE_2", payload: 1000})
 	}
 }
